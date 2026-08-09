e424df3d86adc693e74240a121c82ea3398a4b3c
furtarball <61846505+furtarball@users.noreply.github.com>
2026-08-09 21:54:51 +0200

    Add AppImage GitHub Action

--- appimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeuxo pipefail
-meson configure -Dprefix=/usr
-DESTDIR=AppDir meson install
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/latest/download/linuxdeploy-plugin-appimage-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy/releases/latest/download/linuxdeploy-x86_64.AppImage
-chmod +x linuxdeploy-plugin-appimage-x86_64.AppImage
-chmod +x linuxdeploy-x86_64.AppImage
-NO_STRIP=true ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
